a32b9447bf5d54ae8ac26940bdab046563def719
dvobabsi <dvobabsi@gmail.com>
2024-10-21 22:22:27 +0300

    feat: add table-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -28,24 +28,26 @@ describe('simpleCalculator tests', () => {
 
   test('should exponentiate two numbers', () => {
     // Write your test here
-    const res = simpleCalculator({ a: 5, b: 5, action: Action.Exponentiate });
-    expect(res).toBe(3125);
+    const res = simpleCalculator({ a: 2, b: 3, action: Action.Exponentiate });
+    expect(res).toBe(8);
   });
 
   test('should return null for invalid action', () => {
     // Write your test here
-    const res = simpleCalculator({ a: 2, b: 3, action: '%' });
+    const res = simpleCalculator({ a: 23, b: 23, action: 'InvalidAction' as Action });
     expect(res).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
     // Write your test here
-    const res1 = simpleCalculator({ a: '2', b: 3, action: Action.Add });
-    const res2 = simpleCalculator({ a: 2, b: '3', action: Action.Add });
-    const res3 = simpleCalculator({ a: 2, b: 3, action: 'unknown' });
+    const res1 = simpleCalculator({ a: null, b: 3, action: Action.Add });
+    const res2 = simpleCalculator({ a: 2, b: null, action: Action.Add });
+    const res3 = simpleCalculator({ a: undefined, b: 3, action: Action.Add });
+    const res4 = simpleCalculator({ a: 2, b: undefined, action: Action.Add });
 
     expect(res1).toBeNull();
     expect(res2).toBeNull();
     expect(res3).toBeNull();
+    expect(res4).toBeNull();
   });
 });
